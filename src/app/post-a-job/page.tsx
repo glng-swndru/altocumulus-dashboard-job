a064004cc783682d44181d56ce8b0346d728f2a0
Gilang Swandaru <gilangswandaru27@gmail.com>
2024-08-09 05:23:36 +0800

    formschema

--- src/app/post-a-job/page.tsx
+++ src/app/post-a-job/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+
+import FieldInput from "@/components/organisms/FieldInputs";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Separator } from "@/components/ui/separator";
+import { JOBTYPES } from "@/constants";
 import { jobFormSchema } from "@/lib/form-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowLeftIcon } from "lucide-react";
 import React, { FC } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -18,7 +42,159 @@ const PostJobPage: FC<PostJobPageProps> = ({}) => {
     console.log(val);
   };
 
-  return <div>PostJobPage</div>;
+  return (
+    <div>
+      <div className="inline-flex items-center gap-2 cursor-pointer hover:text-primary">
+        <ArrowLeftIcon className="w-7 h-7" />
+        <span className="text-2xl font-semibold">Post a Job</span>
+      </div>
+
+      <div className="my-5">
+        <div className="text-lg font-semibold">Basic Information</div>
+        <div className="text-gray-400">
+          List out your top perks and benefits.
+        </div>
+      </div>
+
+      <Separator />
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-5 space-y-6 pt-6"
+        >
+          <FieldInput
+            title="Job Title"
+            subtitle="Job titles must be describe one position"
+          >
+            <FormField
+              control={form.control}
+              name="roles"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      className="w-[450px]"
+                      placeholder="e.g. Software Engineer"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>At least 80 characters</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FieldInput>
+
+          <FieldInput
+            title="Type of Employment"
+            subtitle="You can select multiple type of employment"
+          >
+            <FormField
+              control={form.control}
+              name="jobType"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      {JOBTYPES.map((item: string, i: number) => (
+                        <FormItem
+                          key={item + i}
+                          className="flex items-center space-x-3 space-y-0"
+                        >
+                          <FormControl>
+                            <RadioGroupItem value={item} />
+                          </FormControl>
+                          <FormLabel className="font-normal">{item}</FormLabel>
+                        </FormItem>
+                      ))}
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FieldInput>
+
+          <FieldInput
+            title="Salary"
+            subtitle="Please specify the estimated salary range for the role."
+          >
+            <div className="w-[450px] flex flex-row justify-between items-center">
+              <FormField
+                control={form.control}
+                name="salaryFrom"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input className="w-full" placeholder="$100" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <span className="text-center">To</span>
+              <FormField
+                control={form.control}
+                name="salaryTo"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        className="w-full"
+                        placeholder="$1000"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </FieldInput>
+
+          <FieldInput
+            title="Categories"
+            subtitle="You can select job categories"
+          >
+            <FormField
+              control={form.control}
+              name="categoryId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Select job categories</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="w-[450px]">
+                        <SelectValue placeholder="Select job categories" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="m@example.com">
+                        m@example.com
+                      </SelectItem>
+                      <SelectItem value="m@google.com">m@google.com</SelectItem>
+                      <SelectItem value="m@support.com">
+                        m@support.com
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </FieldInput>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default PostJobPage;
